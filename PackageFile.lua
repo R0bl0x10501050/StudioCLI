0ac49ab9de16c376c7095bc9f041e375090214c5
R0bl0x10501050 <74582614+R0bl0x10501050@users.noreply.github.com>
2021-08-28 01:14:21 -0700

    Create PackageFile.lua

---
+++ PackageFile.lua
@@ -0,0 +1,13 @@
+return {
+	['name'] = 'StudioCLI',
+	['version'] = '1.0.0',
+	['description'] = 'A Studio terminal complete with a package manager!',
+	['keywords'] = 'plugin, cli',
+	['author'] = 'R0bl0x10501050',
+	['scripts'] = {
+		
+	},
+	['bugs'] = {},
+	['funding'] = {},
+	['private'] = false,
+}
