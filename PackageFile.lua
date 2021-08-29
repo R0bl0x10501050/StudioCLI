67c59e0aaa9620a3466a8a41ecfe45b635b58b1d
R0bl0x10501050 <74582614+R0bl0x10501050@users.noreply.github.com>
2021-08-28 19:56:15 -0700

    Update PackageFile.lua

--- PackageFile.lua
+++ PackageFile.lua
@@ -1,6 +1,6 @@
 return {
 	['name'] = 'StudioCLI',
-	['version'] = '1.0.0',
+	['version'] = '1.1.0',
 	['description'] = 'A Studio terminal complete with a package manager!',
 	['keywords'] = 'plugin, cli',
 	['author'] = 'R0bl0x10501050',
